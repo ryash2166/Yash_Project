6549be94d68c1a922838449e9fc318cc8d0c413d
ryash2166 <rakholiyayash10@gmail.com>
2024-04-29 12:06:06 +0530

    29/04/2024 (Main Page Complete)

--- emart/src/Components/Tools.jsx
+++ emart/src/Components/Tools.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+// import url from '../assets/Tools.webp'
 
 const Tools = () => {
   return (
-    <div className='overflow-hidden'>
-      <div className='m-0 p-0 max-lg:scale-110'>
-            <img src="https://emart.wpthemedemos.com/plant-garden/wp-content/uploads/sites/10/2022/12/full-bg-img-scaled.webp" alt="" />
+    <div className=''>
+      <div className="bg-cover relative bg-[url('https://emart.wpthemedemos.com/plant-garden/wp-content/uploads/sites/10/2022/12/full-bg-img-scaled.webp')] h-[426px]">
+      <div className='flex m-auto absolute z-10 h-full max-xl:backdrop-brightness-50 max-md:backdrop-brightness-[.3]'>
+      <div className=' mx-[56px] h-[196px] flex justify-end m-auto  max-xl:mx-auto'>
+          <div className='ml-[997px] max-2xl:ml-[720px] max-xl:ml-[630px] max-lg:ml-[330px] max-md:ml-[30px] max-sm:ml-0'>
+            <div className='pl-[72px] pr-[101px] max-xl:p-0 max-md:px-[40px]'>
+              <div className='text-white mr-[10px] max-md:mr-0'>
+                <h2 className='mb-[8px] text-white font-[Jost] text-[45px] max-md:text-[30px] max-md:text-center'>
+                  Find your perfect gardening <br className='max-2xl:hidden' /> tools on best price.
+                </h2>
+              </div>
+            </div>
+            <div className='ml-[74px] max-xl:ml-0 max-md:flex max-md:justify-center'>
+              <div className='text-[17px] pt-[30px] now'>
+                <div className="Shop">
+                    <a href="/" className="px-[40px] py-[12px]" role="button">
+                        <span className="font-normal">shop now</span>
+                    </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        </div> 
+      </div>
     </div>
   )
 }
