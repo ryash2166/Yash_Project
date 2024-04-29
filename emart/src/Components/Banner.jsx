281d7c590ca3042bd5f7e9f706fba8b8e5fef737
ryash2166 <rakholiyayash10@gmail.com>
2024-04-29 16:17:03 +0530

    29/04/2024 (Main Page Complete)

--- emart/src/Components/Banner.jsx
+++ emart/src/Components/Banner.jsx
@@ -4,23 +4,23 @@ function Banner() {
   return (
     <div className="overflow-hidden">
       <section className=" pt-[90px] py-[15px] pb-[109px] max-md:pb-[40px] text-center overflow-hidden ">
-        <div className="mx-[55px] flex max-md:block text-center max-xl:text-start max-2xl:mx-[15px]">
-          <div className=" flex  items-center justify-center text-wrap text-center max-lg:text-start pr-[100px] max-lg:pr-0">
+        <div className="mx-[50px] flex max-md:block text-center max-2xl:mx-[15px]">
+          <div className=" flex  items-center justify-center text-wrap text-center pr-[100px] max-lg:pr-0">
             <div className="">
               <div className="">
                 <div className="">
-                  <div className="max-md:text-[50px] max-lg:text-[66px] text-[88px] mb-[25px]">
+                  <div className="max-md:text-[50px] max-lg:text-[66px] max-lg:text-left max-md:text-center text-[88px] mb-[25px]">
                     <h1 className="">Plant a tree for better world</h1>
                   </div>
                 </div>
               </div>
               <div className="mb-[56px]  max-md:mb-[45px]">
-                <div className="text-[16px] font-light max-md:text-[15px] text-muted">
+                <div className="text-[16px] font-light max-md:text-[15px] text-muted max-lg:text-left max-md:text-center">
                   Rectangle follower distribute device layer. Library hand ellipse device strikethrough.
                 </div>
               </div>
               <div className="">
-                <div className="">
+                <div className="max-lg:text-left max-md:text-center">
                   <div className="shop">
                     <a href="/" className="border-1 border-black px-[40px] py-[12px]" role="button">
                         <span className="font-normal">shop now</span>
@@ -52,7 +52,7 @@ function Banner() {
                   <img
                     decoding="async"
                     src="https://emart.wpthemedemos.com/plant-garden/wp-content/uploads/sites/10/2022/12/plants-banner-2nd.webp"
-                    className="w-[950px] max-lg:w-[450px] max-xl:w-[600px] max-md:w-auto hover:pl-[10px] duration-300 ease-in-out"
+                    className="w-[850px] max-lg:w-[450px] max-xl:w-[600px] max-md:w-auto hover:pl-[10px] duration-300 ease-in-out"
                     loading="lazy"
                   />
                 </div>
