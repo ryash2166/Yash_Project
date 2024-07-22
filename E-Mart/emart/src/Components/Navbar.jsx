2326c7106961435c9ed87dd49788d87c8be32c36
ryash2166 <rakholiyayash10@gmail.com>
2024-07-22 15:35:15 +0530

    bug updated

--- E-Mart/emart/src/Components/Navbar.jsx
+++ E-Mart/emart/src/Components/Navbar.jsx
@@ -890,24 +890,6 @@ function Navbar1() {
                     </svg>
                   </button>
                 )}
-
-                {/* <NavLink to='profile'>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-user"
-                  >
-                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                </NavLink> */}
               </div>
               <div className="max-xl:hidden">
                 <NavLink to="wishlist">
